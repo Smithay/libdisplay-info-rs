1e469aae017adb17c1fe8936cedd606014395984
Christian Meissl <meissl.christian@gmail.com>
2025-01-06 19:29:02 +0100

    sys: do not link against native lib in doc build
    the libdisplay-info native library is not available
    in the docs.rs build environment.
    we can just skip linking in this case as we do not
    need it for building the documentation

--- libdisplay-info-sys/build.rs
+++ libdisplay-info-sys/build.rs
@@ -1,4 +1,11 @@
 fn main() {
+    if std::env::var("DOCS_RS").is_ok() {
+        // don't link against unavailable native lib in doc.rs builds and
+        // just assume the highest version is available
+        println!("cargo:rustc-cfg=feature=\"v0_2\"");
+        return;
+    }
+
     let deps = system_deps::Config::new().probe().unwrap();
     let native_lib = deps.get_by_name("libdisplay-info").unwrap();
     let native_version = semver::Version::parse(&native_lib.version).unwrap();
